ceda1c523899256a946a35d11942e2c06ac8824f
Shash <ashishsinghlogic@gmail.com>
2024-10-25 22:30:29 +0530

    button bottom sheet added

--- wallep/app/(tabs)/account.tsx
+++ wallep/app/(tabs)/account.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+
+import { Bottom } from "@/components/BottomSheet";
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function account() {
+    const[pictureOpen, setPictureOpen] = useState(false);
     return (
         <SafeAreaView>
             <Text> Accounts</Text>
+            <Button title ="open bottom sheet" onPress={() =>{
+                setPictureOpen(true);
+            }} /> 
+
+            {pictureOpen && <Bottom />}
+
         </SafeAreaView>
     )
 }
